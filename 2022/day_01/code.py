6c944d6d59bb3a155085dd0530709c31bc302c56
dwwest <31937638+dwwest@users.noreply.github.com>
2022-12-01 11:12:45 -0500

    day 1

--- 2022/day_01/code.py
+++ 2022/day_01/code.py
@@ -1,3 +1,25 @@
 import numpy as np
 
 
+""" READ DATA """
+raw_data = []
+with open('input.txt', 'r') as f:
+    for line in f:
+        raw_data.append(line.strip('\n'))
+
+""" PART ONE """
+elves = []
+current = 0
+for line in raw_data:
+    if line == '':
+        elves.append(current)
+        current = 0
+    else:
+        current += int(line)
+
+print(np.max(elves))
+
+""" PART TWO """
+
+elves.sort(reverse=True)
+print(np.sum(elves[0:3]))
