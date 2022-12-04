32a008868fc0642e080e85ceec798b94685c7d96
Devany West <dwest@rockefeller.edu>
2022-12-04 12:31:54 -0500

    recommiting day 4 after some critique from bcc32 :3

--- 2022/day_04/code.py
+++ 2022/day_04/code.py
@@ -11,13 +11,13 @@ with open('input.txt','r') as f:
 subsets = 0
 overlaps = 0
 for a in assigns:
-    set_one = set([i for i in range(a[0][0],a[0][1]+1)])
-    set_two = set([i for i in range(a[1][0],a[1][1]+1)])
+    set_one = set(range(a[0][0],a[0][1]+1))
+    set_two = set(range(a[1][0],a[1][1]+1))
     """ PART ONE"""
     if set_one.issubset(set_two) or set_two.issubset(set_one):
         subsets += 1
     """ PART TWO """
-    if len(list(set_one.intersection(set_two))) != 0:
+    if set_one.intersection(set_two):
         overlaps += 1
 
 print(subsets)
